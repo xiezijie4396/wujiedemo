0b783f967ad5ad9f724462f96524776a1b9f823c
xiezijie4396 <775826360@qq.com>
2023-08-18 17:33:50 +0800

    feat: 初始化项目

---
+++ vue3/vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
+module.exports = defineConfig({
+  publicPath: '/child/vue2/',
+  transpileDependencies: true,
+  devServer: {
+    port: 8003,
+    headers: {
+      'Access-Control-Allow-Origin': '*' // 主应用获取子应用时跨域响应头
+    }
+  },
+  outputDir: path.join('../../server/child/vue2'),
+})
---
+++ vuevite/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 8001,
+    cors: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*' // 主应用获取子应用时跨域响应头
+    }
+  },
+  build: {
+    outDir: path.join(__dirname, '../server/main')
+  }
+})
